02ced9a90ce1ea80b03e2515ac561dfd93f196f2
timaca <15361931618@163.com>
2019-11-21 12:02:37 +0800

    完成simi接口

--- src/main/java/com/roomaja/neteasecloudmusicapi/controller/SimiController.java
+++ src/main/java/com/roomaja/neteasecloudmusicapi/controller/SimiController.java
@@ -2,7 +2,6 @@ package com.roomaja.neteasecloudmusicapi.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.roomaja.neteasecloudmusicapi.service.SimiService;
-import com.roomaja.neteasecloudmusicapi.service.SongService;
 import com.roomaja.neteasecloudmusicapi.util.CookieUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -39,6 +38,50 @@ public class SimiController {
                                    HttpServletResponse response,
                                    HttpServletRequest request) {
         ResponseEntity<JSONObject> result = simiService.simiPlaylist(
+                id,
+                Integer.valueOf(limit),
+                Integer.valueOf(offset),
+                cookieUtil.getCookies(request));
+        CookieUtil.setCookie(result.getHeaders(), response);
+        return result.getBody();
+    }
+
+    /* 获得相似mv */
+    @RequestMapping(value = "/mv")
+    public JSONObject simiMv(@RequestParam String mvid,
+                                   HttpServletResponse response,
+                                   HttpServletRequest request) {
+        ResponseEntity<JSONObject> result = simiService.simiMv(
+                mvid,
+                cookieUtil.getCookies(request));
+        CookieUtil.setCookie(result.getHeaders(), response);
+        return result.getBody();
+    }
+
+    /* 获得相似歌曲 */
+    @RequestMapping(value = "/song")
+    public JSONObject simiSong(@RequestParam String id,
+                                   @RequestParam(defaultValue = "50") String limit,
+                                   @RequestParam(defaultValue = "0") String offset,
+                                   HttpServletResponse response,
+                                   HttpServletRequest request) {
+        ResponseEntity<JSONObject> result = simiService.simiSong(
+                id,
+                Integer.valueOf(limit),
+                Integer.valueOf(offset),
+                cookieUtil.getCookies(request));
+        CookieUtil.setCookie(result.getHeaders(), response);
+        return result.getBody();
+    }
+
+    /* 获取最近 5 个听了这首歌的用户 */
+    @RequestMapping(value = "/user")
+    public JSONObject simiUser(@RequestParam String id,
+                                   @RequestParam(defaultValue = "50") String limit,
+                                   @RequestParam(defaultValue = "0") String offset,
+                                   HttpServletResponse response,
+                                   HttpServletRequest request) {
+        ResponseEntity<JSONObject> result = simiService.simiUser(
                 id,
                 Integer.valueOf(limit),
                 Integer.valueOf(offset),
--- src/main/java/com/roomaja/neteasecloudmusicapi/service/SimiService.java
+++ src/main/java/com/roomaja/neteasecloudmusicapi/service/SimiService.java
@@ -1,6 +1,5 @@
 package com.roomaja.neteasecloudmusicapi.service;
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.roomaja.neteasecloudmusicapi.config.Constant;
 import com.roomaja.neteasecloudmusicapi.util.CryptoUtil;
@@ -44,11 +43,10 @@ public class SimiService {
      * @param cookies
      * @return
      */
-    public ResponseEntity<JSONObject> simiPlaylist(String id, Integer limit, Integer offset, Map<String, String> cookies) {
+    public ResponseEntity<JSONObject> simiPlaylist(String id, Integer limit, Integer offset,
+            Map<String, String> cookies) {
 
         JSONObject params = new JSONObject();
-
-        JSONArray c = new JSONArray();
 
         params.put("songid", id);
         params.put("limit", limit);
@@ -59,6 +57,70 @@ public class SimiService {
 
         return RestTemplateUtil.postWeapi(encrypt[0], encrypt[1],
                 Constant.NETEASE_BASE_URL + "/weapi/discovery/simiPlaylist?csrf_token=", cookies, restTemplate);
+    }
+
+    /**
+     * 获得相似mv
+     *
+     * @param mvid    mv id
+     * @param cookies
+     * @return
+     */
+    public ResponseEntity<JSONObject> simiMv(String mvId, Map<String, String> cookies) {
+
+        JSONObject params = new JSONObject();
+
+        params.put("mvid", mvId);
+        params.put("csrf_token", cookies.get("__csrf"));
+
+        String[] encrypt = CryptoUtil.weapiEncrypt(params.toJSONString());
+
+        return RestTemplateUtil.postWeapi(encrypt[0], encrypt[1],
+                Constant.NETEASE_BASE_URL + "/weapi/discovery/simiMV?csrf_token=", cookies, restTemplate);
+    }
+
+    /**
+     * 获得相似歌曲
+     *
+     * @param id      歌曲 id
+     * @param cookies
+     * @return
+     */
+    public ResponseEntity<JSONObject> simiSong(String id, Integer limit, Integer offset, Map<String, String> cookies) {
+
+        JSONObject params = new JSONObject();
+
+        params.put("songid", id);
+        params.put("limit", limit);
+        params.put("offset", offset);
+        params.put("csrf_token", cookies.get("__csrf"));
+
+        String[] encrypt = CryptoUtil.weapiEncrypt(params.toJSONString());
+
+        return RestTemplateUtil.postWeapi(encrypt[0], encrypt[1],
+                Constant.NETEASE_BASE_URL + "/weapi/discovery/simiSong?csrf_token=", cookies, restTemplate);
+    }
+
+    /**
+     * 获取最近 5 个听了这首歌的用户
+     *
+     * @param id      歌曲 id
+     * @param cookies
+     * @return
+     */
+    public ResponseEntity<JSONObject> simiUser(String id, Integer limit, Integer offset, Map<String, String> cookies) {
+
+        JSONObject params = new JSONObject();
+
+        params.put("songid", id);
+        params.put("limit", limit);
+        params.put("offset", offset);
+        params.put("csrf_token", cookies.get("__csrf"));
+
+        String[] encrypt = CryptoUtil.weapiEncrypt(params.toJSONString());
+
+        return RestTemplateUtil.postWeapi(encrypt[0], encrypt[1],
+                Constant.NETEASE_BASE_URL + "/weapi/discovery/simiUser?csrf_token=", cookies, restTemplate);
     }
 
 }
